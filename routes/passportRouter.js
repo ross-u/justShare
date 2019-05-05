e712f9ad92adef69b472ec704808d037798c2e0b
Katharine Cooney <katharinecooney@gmail.com>
2019-05-05 18:45:54 +0200

    Update landing page background image

--- routes/passportRouter.js
+++ routes/passportRouter.js
@@ -9,4 +9,11 @@ router.get('/signup', (req, res, next) => {
   res.render('passport/signup');
 });
 
+//* ********************* LOGIN *****************************//
+
+// GET  '/login'
+router.get('/login', (req, res, next) => {
+  res.render('passport/login');
+});
+
 module.exports = router;
