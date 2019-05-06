78136bb7044213b1492f1a9f2e74ccb3450c0985
Gianfranco Rusca <gruscaa@gmail.com>
2019-05-06 17:31:26 +0200

    recent changes

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,8 @@ router.use('/', authRouter);
 router.use('/product', prodRouter);
 
 /* GET home page. */
-<<<<<<< HEAD
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'justShare' });
-=======
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
->>>>>>> 13183e9153117d8d272f09f149f5e721a8b56817
 });
 
 module.exports = router;
