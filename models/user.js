4d6f2681863ccc4e364465a8c9aada14f3b25d6f
Gianfranco Rusca <gruscaa@gmail.com>
2019-05-07 12:01:14 +0200

    mabox start

--- models/user.js
+++ models/user.js
@@ -5,13 +5,20 @@ const userSchema = new Schema({
   username: String,
   password: String,
   restaurant: String,
-  location: String
+  location: {
+    type: {
+      type: String
+    },
+    coordinates: [Number]
+  }
 }, {
   timestamps: { 
     createdAt: 'created_at',
     updatedAt: 'updated_at'
   }
 });
+
+userSchema.index({ location: '2dsphere' });
 
 const User = mongoose.model('User', userSchema);
 
