e08f3a6e4d86f6772ca1ca4a40b1faa679089924
Gianfranco Rusca <gruscaa@gmail.com>
2019-05-07 09:13:54 +0200

    new changes

--- routes/prodRoutes.js
+++ routes/prodRoutes.js
@@ -18,6 +18,29 @@ router.post('/new', (req, res, next) => {
     .catch(error => res.redirect('/new'));
 });
 
+// Edit product
+
+router.get('/edit', (req, res, next) => {
+  Product.findOne({ _id: req.query.product_id })
+    .then(product => res.render('passport/edit', { product }))
+    .catch(error => console.log(error));
+});
+
+router.post('/edit', (req, res, next) => {
+  const { name, description, allergens, location } = req.body;
+
+  Product.update({ _id: req.query.product_id }, { $set: { name, description, allergens, location } }, { new: true })
+    .then(product => res.redirect('/profile'))
+    .catch(error => console.log(error));
+});
+
+// Delete product
+router.get('/delete', (req, res, next) => {
+  Product.deleteOne( { _id: req.query.product_id })
+    .then(product => res.redirect('/profile'))
+    .catch(err => console.log(err));
+});
+
 // See all Products
 
 router.get('/', (req, res, next) => {
